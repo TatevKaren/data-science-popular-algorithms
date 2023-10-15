4481b6a4052b7389806a1bf95dff828e2511620e
TK_Aslanyan <76843403+TatevKaren@users.noreply.github.com>
2023-10-15 13:24:37 -0700

    Add files via upload

---
+++ Supervised Learning Algorithms/XGBoost/XGBoost.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+
+# Seed for reproducibility
+np.random.seed(42)
+
+# Generate synthetic data
+num_samples = 200
+num_rooms = np.random.randint(3, 10, num_samples)
+house_age = np.random.randint(1, 100, num_samples)
+noise = np.random.normal(0, 50, num_samples)
+
+# Assume a linear relation with price = 50*rooms + 0.5*age + noise
+price = 50*num_rooms + 0.5*house_age + noise
+
+# Create DataFrame
+data = pd.DataFrame({'num_rooms': num_rooms, 'house_age': house_age, 'price': price})
+
+# Plot
+plt.scatter(data['num_rooms'], data['price'], label='Num Rooms vs Price', color = 'forestgreen')
+plt.scatter(data['house_age'], data['price'], label='House Age vs Price', color = 'darkred')
+plt.xlabel('Feature Value')
+plt.ylabel('Price')
+plt.legend()
+plt.title('Scatter Plots of Features vs Price')
+plt.show()
+
+# Splitting data into training and testing sets
+X = data[['num_rooms', 'house_age']]
+y = data['price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train XGBoost model
+model_xgb = xgb.XGBRegressor(objective ='reg:squarederror', n_estimators = 100, seed = 42)
+model_xgb.fit(X_train, y_train)
+
+# Predictions
+predictions = model_xgb.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, predictions)
+rmse = np.sqrt(mse)
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"Root Mean Squared Error: {rmse:.2f}")
+
+# Visualization: Actual vs Predicted Prices
+plt.scatter(y_test, predictions, color="forestgreen")
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=3)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.title('Actual vs Predicted House Prices with XGBoost')
+plt.show()
